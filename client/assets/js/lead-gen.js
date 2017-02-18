e9c6db986591d53770ab0e71a7018b9c80fe4559
Vlad Berteanu <vlad.berteanu@gmail.com>
2017-02-18 13:44:58 -0800

    more work

--- client/assets/js/lead-gen.js
+++ client/assets/js/lead-gen.js
@@ -1,3 +1,6 @@
+var account = null;
+
+
 function getFormData(formId) {
     var selectorInput = '#' + formId + ' input[name='
     var selectorTextArea = '#' + formId + ' textarea'
@@ -36,6 +39,21 @@ function displayRegistrationError(formId, errorMessage) {
 }
 
 
+function setReferralLink() {
+    var referralLink = 'onehundred.me/?ref=' + (account ? account.referralLink : null);
+    var referralLinkDom = $('.referral-link a')
+    referralLinkDom.html(referralLink)
+    referralLinkDom.attr('href', 'http://' + referralLink)
+}
+
+
+function displayRegistrationSuccess() {
+    setReferralLink()
+    $('.signup-box').hide()
+    $('.signup-success-box').fadeIn()
+}
+
+
 function register(formId) {
     var errorBox = getErrorBox(formId)
     var formData = getFormData(formId)
@@ -57,9 +75,73 @@ function register(formId) {
         success: function (response) {
             if (response.error) {
                 displayRegistrationError(formId, response.error)
+            } else {
+                account = response.lead
+                displayRegistrationSuccess()
             }
         },
    });
-
     return false;
+}
+
+
+function getParameterByName(name, url) {
+    if (!url) {
+      url = window.location.href
+    }
+    name = name.replace(/[\[\]]/g, "\\$&")
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)")
+    var results = regex.exec(url)
+    if (!results) {
+        return null
+    }
+    if (!results[2]) {
+        return ''
+    }
+    return decodeURIComponent(results[2].replace(/\+/g, " "))
+}
+
+
+function displayReferralBar(friendName) {
+    var referralBar = $('#referral-bar')
+    referralBar.find('strong').html(friendName)
+    referralBar.show()
+    $('.menu-bar').addClass('with-referral')
+}
+
+
+
+function closeReferralBar() {
+    $('.menu-bar').removeClass('with-referral')
+}
+
+
+function loadReferralBar() {
+    var referralLink = getParameterByName('ref')
+
+    if (!referralLink) {
+        return
+    }
+
+    $.ajax({
+        type: "GET",
+        url: "/api/leads?referralLink=" + referralLink,
+        dataType: "json",
+        success: function (response) {
+            if (response.lead) {
+                displayReferralBar(response.lead.name)
+            }
+        },
+   });
+}
+
+
+function shareOnFacebook() {
+    var referralLink = account ? account.referralLink : null;
+
+    FB.ui({
+      method: 'feed',
+      link: 'http://onehundred.me?ref=' + referralLink,
+      caption: 'Start investing $100 monthly',
+    }, function(response){});
 }
